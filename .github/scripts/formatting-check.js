13276eeede12e972db58ad075a39017acf2871b3
Darren Waddell <darren@stepzen.com>
2021-04-26 09:39:22 +0100

    Update github checks

--- .github/scripts/formatting-check.js
+++ .github/scripts/formatting-check.js
@@ -1,28 +1,20 @@
-const core = require("@actions/core")
-const fs = require("fs")
-const glob = require("glob")
-const prettier = require("prettier")
+const core = require("@actions/core");
+const fs = require("fs");
+const glob = require("glob");
+const prettier = require("prettier");
 
 async function run() {
-  const files = glob.sync("**/*.graphql")
+  const files = glob.sync("**/*.graphql");
 
   for (const file of files) {
-    const graphql = fs.readFileSync(file, "utf8")
-
-    const hacked = graphql.replace(
-      /(type\s(\S+)\simplements\s(\S+)\s?{})\n/gi,
-      "type $2 implements $3\n"
-    )
-    const formatted = prettier.format(hacked, { parser: "graphql" })
-    const unhacked = formatted.replace(
-      /(type\s(\S+)\simplements\s(\S+))\n/gi,
-      "type $2 implements $3 {}\n"
-    )
-
-    if (graphql !== unhacked) {
-      throw new Error(`File ${file} not formatted correctly`)
+    const graphql = fs.readFileSync(file, "utf8");
+    const formatted = prettier.format(graphql, { parser: "graphql" });
+    if (graphql !== formatted) {
+      throw new Error(
+        `File ${file} has an error. Please check it is valid GraphQL and formatted, by running 'prettier'`
+      );
     }
   }
 }
 
-run().catch((e) => core.setFailed(e))
+run().catch((e) => core.setFailed(e));
